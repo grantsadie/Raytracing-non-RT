d3266b225642edd6811f8d2fe283dda0effa0b78
Grant Sadie <grantdsadie@gmail.com>
2023-03-19 14:53:14 +1100

    Updated debugger launch.json

--- scripts/linux_build.sh
+++ scripts/linux_build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Name of the build directory
 BUILD_DIR="build_debug_linux"
 
 # Create the build directory if it doesn't exist
@@ -8,13 +7,9 @@ if [ ! -d "$BUILD_DIR" ]; then
     mkdir "$BUILD_DIR"
 fi
 
-# Change to the build directory
 cd "$BUILD_DIR"
 
-# Run CMake to generate the build system files
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 
-# Build the project using the generated build system files
 cmake --build .
 
-# Optionally, run any tests or execute the built binary here
---
+++ src/raytracings/ray.cpp
@@ -0,0 +1,1 @@
+#include "raytracings/ray.h"

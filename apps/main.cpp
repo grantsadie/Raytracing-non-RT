0687e8b1ae5913b3f308d74be7ebdea20072c5ef
Grant Sadie <grantdsadie@gmail.com>
2023-03-25 19:25:49 +1100

    Antialiasing

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,5 +1,9 @@
+#include "raytracings/rtutils.h"
 #include "colour/colour.h"
 #include "raytracings/ray.h"
+#include "raytracings/hittable_list.h"
+#include "raytracings/sphere.h"
+#include "raytracings/camera.h"
 #include "math/vec3.h"
 
 #include <iostream>
@@ -21,51 +25,44 @@
 //     }
 // }
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant) ) / a;
+color ray_color(const ray& r, const hittable& world) {
+    // auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    // if (t > 0.0) {
+    //     vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+    //     return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    // }
+    // vec3 unit_direction = unit_vector(r.direction());
+    // t = 0.5*(unit_direction.y() + 1.0);
+    // return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.5, 0.5);
+    // World items
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
-}
 
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
-    }
+    // Background
     vec3 unit_direction = unit_vector(r.direction());
-    // std::cerr <<  unit_direction.y() << ' ' << std::endl;
-    t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.5, 0.5);
+    double t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
 int main() {
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    // const int image_width = 16000;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+
+    // World 
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
 
     // Camera
-
-    auto viewport_height = 2.0; // could be metres. Just in real world units.
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-
+    camera cam;
+    
     // Render
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -73,12 +70,14 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1); // From 0 to 1, left to right
-            auto v = double(j) / (image_height-1);
-            // Below will be the position of the pixel in the viewport. The ray I guess as a 3D vec?
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin); 
-            color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
+            color pixel_colour(0,0,0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = double(i + random_double()) / (image_width-1); // From 0 to 1, left to right
+                auto v = double(j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_colour += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_colour, samples_per_pixel);
         }
     }
 

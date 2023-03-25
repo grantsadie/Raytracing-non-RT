d509db9beaa019e8f493c03603379a9d2aa257ae
Grant Sadie <grantdsadie@gmail.com>
2023-03-25 12:40:50 +1100

    Simplified math

--- apps/main.cpp
+++ apps/main.cpp
@@ -4,16 +4,54 @@
 
 #include <iostream>
 
+// double hit_sphere(const point3& center, double radius, const ray& r) {
+//     // center is the center of the circle. 0,0,-1
+//     // r.origin is 0,0,0 cause its the origin of the ray
+//     // oc is the difference in the rays origin and the spheres center
+//     // oc is pointing from the center of the sphere to the origin of the ray
+//     vec3 oc = r.origin() - center;
+//     auto a = dot(r.direction(), r.direction());
+//     auto b = 2.0 * dot(oc, r.direction());
+//     auto c = dot(oc, oc) - radius * radius;
+//     auto discriminant = b*b - 4*a*c;
+//     if (discriminant < 0) {
+//         return -1.0;
+//     } else {
+//         return (-b - sqrt(discriminant) ) / (2.0*a);
+//     }
+// }
+
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-half_b - sqrt(discriminant) ) / a;
+    }
+}
+
 color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    // std::cerr <<  unit_direction.y() << ' ' << std::endl;
+    t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.5, 0.5);
 }
 
 int main() {
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
+    // const int image_width = 16000;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 

00bb8f9c60ce08c18d3a4dd14a65edd4d4064f65
Grant Sadie <grantdsadie@gmail.com>
2023-03-26 22:57:09 +1100

    Updated build script

--- scripts/linux_build.sh
+++ scripts/linux_build.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
 BUILD_DIRS="build_dirs"
-BUILD_LINUX_DEBUG="build_debug_linux"
 
-# Create the build directory if it doesn't exist
+print_help() {
+    echo "Usage: $0 --debug | --release"
+    echo ""
+    echo "Options:"
+    echo "  --debug   Build in Debug mode."
+    echo "  --release Build in Release mode."
+}
+
+if [ $# -eq 0 ]; then
+    print_help
+    exit 1
+fi
+
+BUILD_MODE=""
+if [ "$1" = "--debug" ]; then
+    BUILD_MODE="Debug"
+elif [ "$1" = "--release" ]; then
+    BUILD_MODE="Release"
+else
+    echo "Invalid argument: $1"
+    print_help
+    exit 1
+fi
+
+BUILD_TARGET="build_${BUILD_MODE,,}_linux"
+
 if [ ! -d "$BUILD_DIRS" ]; then
     mkdir "$BUILD_DIRS"
 fi
 cd "$BUILD_DIRS"
 
-if [ ! -d "$BUILD_LINUX_DEBUG" ]; then
-    mkdir "$BUILD_LINUX_DEBUG"
+if [ ! -d "$BUILD_TARGET" ]; then
+    mkdir "$BUILD_TARGET"
 fi
-cd "$BUILD_LINUX_DEBUG"
+cd "$BUILD_TARGET"
 
-cmake -DCMAKE_BUILD_TYPE=Debug ../..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_MODE ../..
 
 cmake --build .
-
